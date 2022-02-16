b25c109e22a9cee0c6f3f38ae205af72445b7f7b
Mohamed Sabek <51997284+Sabek99@users.noreply.github.com>
2022-02-16 20:28:13 +0200

    adding EF core and making the first migeration

--- Startup.cs
+++ Startup.cs
@@ -2,16 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Services.CharacterService;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace _Net_Core_web_API
 {
@@ -28,6 +31,7 @@ namespace _Net_Core_web_API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddSwaggerGen(c =>
